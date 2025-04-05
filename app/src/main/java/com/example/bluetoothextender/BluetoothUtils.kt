29972dc29bf3cfa716d56463aa6a638b79d1f0bd
Anand Doshi <anand.doshi20@imperial.ac.uk>
2025-04-05 12:13:47 +0100

    Corrected BluetoothUtils to work by taking in a context and an activity.

--- app/src/main/java/com/example/bluetoothextender/BluetoothUtils.kt
+++ app/src/main/java/com/example/bluetoothextender/BluetoothUtils.kt
@@ -1,23 +1,27 @@
+import android.app.Activity
 import android.bluetooth.*
+import android.content.Context
 import android.content.Intent
 import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.content.ContextCompat.getSystemService
 
-class BluetoothUtils {
+class BluetoothUtils(val context: Context, val activity: Activity) {
 
-    val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
-    val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+    val bluetoothManager: BluetoothManager? = getSystemService(context, BluetoothManager::class.java)
+    val bluetoothAdapter: BluetoothAdapter? = bluetoothManager?.getAdapter()
 
     public fun setup() {
         if (bluetoothAdapter == null) {
             System.exit(0)
         }
 
-        var RESULT_ENABLE_BT: int = 0
+        var RESULT_ENABLE_BT: Int = 0
 
         if (bluetoothAdapter?.isEnabled == false) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, RESULT_ENABLE_BT)
+            // starts a sub activity from activity with the passed intent, i.e. to enable bluetooth.
+            // when subactivity exits, it returns RESULT_ENABLE_BT to activity's onActivityResult() for processing
+            startActivityForResult(activity, enableBtIntent, RESULT_ENABLE_BT, null)
         }
     }
 
