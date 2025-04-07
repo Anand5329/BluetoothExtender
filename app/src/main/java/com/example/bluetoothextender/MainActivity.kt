5f70aee89d00266d300256361635365cf307c8fb
Anand Doshi <anand.doshi20@imperial.ac.uk>
2025-04-07 19:04:52 +0100

    Testing onActivityCallback within MainActivity.

--- app/src/main/java/com/example/bluetoothextender/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothextender/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.bluetoothextender
 
 import BluetoothUtils
+import android.Manifest
 import android.app.ComponentCaller
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
 import android.companion.CompanionDeviceManager
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -17,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.bluetoothextender.ui.theme.BluetoothExtenderTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,9 +42,36 @@ class MainActivity : ComponentActivity() {
             }
         }
         Log.v(TAG, "Starting setup of bluetooth...")
-        btUtils.ensureBluetoothEnabled()
-        btUtils.setupCompanionDeviceSearch()
+//        btUtils.ensureBluetoothEnabled()
+//        TODO("check permissions and ask to turn on")
+        ensureBluetoothEnabled()
+//        btUtils.setupCompanionDeviceSearch()
         Log.v(TAG, "Bluetooth setup done!")
+    }
+
+    fun ensureBluetoothEnabled() {
+        val bluetoothManager: BluetoothManager? =
+            this.getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager?.adapter
+
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        // starts a sub activity from activity with the passed intent, i.e. to enable bluetooth.
+        // when subactivity exits, it returns RESULT_ENABLE_BT to activity's onActivityResult() as requestCode for processing
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        startActivityForResult(enableBtIntent, BluetoothUtils.RESULT_ENABLE_BT, null)
     }
 
     override fun onActivityResult(
